b951ea92c4cf6b6725572e06290f17cce24ebe3a
Beat Scherrer <b.scherrer@mt-robot.com>
2022-07-18 23:37:57 +0200

    dim middle status part

--- lua/lualine/themes/gravel_pit.lua
+++ lua/lualine/themes/gravel_pit.lua
@@ -6,27 +6,27 @@ return {
 	normal = {
 		a = { bg = colors[style].black, fg = colors[style].fg, gui = "bold" },
 		b = { bg = colors[style].comments, fg = colors[style].black },
-		c = { bg = colors[style].black, fg = colors[style].fg },
+		c = { bg = colors[style].black, fg = colors[style].comments },
 	},
 	insert = {
 		a = { bg = colors[style].green, fg = colors[style].black, gui = "bold" },
 		b = { bg = colors[style].comments, fg = colors[style].black },
-		c = { bg = colors[style].black, fg = colors[style].white },
+		c = { bg = colors[style].black, fg = colors[style].comments },
 	},
 	visual = {
 		a = { bg = colors[style].yellow, fg = colors[style].black, gui = "bold" },
 		b = { bg = colors[style].comments, fg = colors[style].black },
-		c = { bg = colors[style].black, fg = colors[style].white },
+		c = { bg = colors[style].black, fg = colors[style].comments },
 	},
 	replace = {
 		a = { bg = colors[style].blue, fg = colors[style].bg, gui = "bold" },
 		b = { bg = colors[style].comments, fg = colors[style].black },
-		c = { bg = colors[style].black, fg = colors[style].white },
+		c = { bg = colors[style].black, fg = colors[style].comments },
 	},
 	command = {
 		a = { bg = colors[style].magenta, fg = colors[style].black, gui = "bold" },
 		b = { bg = colors[style].comments, fg = colors[style].black },
-		c = { bg = colors[style].black, fg = colors[style].white },
+		c = { bg = colors[style].black, fg = colors[style].comments },
 	},
 	inactive = {
 		a = { bg = colors[style].darkgray, fg = colors[style].gray, gui = "bold" },
